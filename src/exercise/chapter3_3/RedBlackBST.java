10fc5265daabf5be47d6b32ee728bfd168492b08
Yyiif <h759233960@163.com>
2018-07-21 16:11:15 +0800

    chapter 3_3 RedBlackBST

--- src/exercise/chapter3_3/RedBlackBST.java
+++ src/exercise/chapter3_3/RedBlackBST.java
@@ -1,5 +1,7 @@
 package exercise.chapter3_3;
 
+import exercise.chapter1_3.Queue;
+
 import java.util.NoSuchElementException;
 
 public class RedBlackBST<Key extends Comparable<Key>, Value> {
@@ -7,41 +9,55 @@ public class RedBlackBST<Key extends Comparable<Key>, Value> {
     private static final boolean BLACK = false;
     private Node root;
 
-    private int size(Node x) {
-        if (x == null) return 0;
-        return x.N;
+    private int size(Node h) {
+        if (h == null) return 0;
+        return h.N;
+    }
+
+    public int size() {
+        return size(root);
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
     }
 
     private boolean isRed(Node h) {
-        return h.color == RED;
+        if (h == null) return false;
+        return h.color;
     }
 
     private Node rotateLeft(Node h) {
+        if (h == null) throw new IllegalArgumentException();
         Node x = h.right;
         h.right = x.left;
         x.left = h;
         x.color = h.color;
         h.color = RED;
         x.N = h.N;
-        h.N = size(h.left) + size(h.right) + 1;
+        h.N = 1 + size(h.left) + size(h.right);
         return x;
     }
 
     private Node rotateRight(Node h) {
+        if (h == null) throw new IllegalArgumentException();
         Node x = h.left;
         h.left = x.right;
         x.right = h;
         x.color = h.color;
         h.color = RED;
         x.N = h.N;
-        h.N = size(h.left) + size(h.right) + 1;
+        h.N = 1 + size(h.left) + size(h.right);
         return x;
     }
 
-    private void flipColors(Node h) {
-        h.left.color = BLACK;
-        h.right.color = BLACK;
-        h.color = RED;
+    private Node flipColors(Node h) {
+        if (h == null || h.left == null || h.right == null) throw new IllegalArgumentException();
+        h.color = !h.color;
+        h.left.color = !h.left.color;
+        h.right.color = !h.right.color;
+
+        return h;
     }
 
     public void put(Key key, Value value) {
@@ -50,97 +66,174 @@ public class RedBlackBST<Key extends Comparable<Key>, Value> {
     }
 
     private Node put(Node h, Key key, Value value) {
+        if (key == null) throw new IllegalArgumentException();
         if (h == null) return new Node(key, value, 1, RED);
-        int cmp = key.compareTo(h.key);
-        if (cmp < 0) put(h.left, key, value);
-        else if (cmp > 0) put(h.right, key, value);
+
+        int comp = key.compareTo(h.key);
+        if (comp > 0) put(h.right, key, value);
+        else if (comp < 0) put(h.left, key, value);
         else h.value = value;
 
         if (isRed(h.right) && !isRed(h.left)) h = rotateLeft(h);
         if (isRed(h.left) && isRed(h.left.left)) h = rotateRight(h);
-        if (isRed(h.left) && isRed(h.right)) flipColors(h);
+        if (isRed(h.left) && isRed(h.right)) h = flipColors(h);
 
         h.N = size(h.left) + size(h.right) + 1;
         return h;
     }
 
-    public int size() {
-        if (root == null) return 0;
-        if (root.N < 0) throw new IllegalArgumentException();
-        return root.N;
+    public void deleteMin() {
+        if (!isRed(root.left) && !isRed(root.right))
+            root.color = RED;
+        root = deleteMin(root);
+        if (!isEmpty()) root.color = BLACK;
     }
 
-    public boolean isEmpty() {
-        return size() == 0;
+    private Node deleteMin(Node h) {
+        if (h.left == null) return null;
+        if (!isRed(h.left) && !isRed(h.left.left))
+            h = moveRedLeft(h);
+        h.left = deleteMin(h.left);
+        return balance(h);
+    }
+
+    private Node moveRedLeft(Node h) {
+        flipColors(h);
+
+        if (isRed(h.right.left)) {
+            h = rotateRight(h);
+            h = rotateLeft(h);
+        }
+        return h;
+    }
+
+    public void deleteMax() {
+        if (!isRed(root.left) && !isRed(root.right))
+            root.color = RED;
+        root = deleteMax(root);
+        if (!isEmpty()) root.color = BLACK;
+    }
+
+    private Node deleteMax(Node h) {
+        if (isRed(h.left))
+            h = rotateRight(h);
+        if (h.right == null) return null;
+
+        if (!isRed(h.right) && !isRed(h.right.left))
+            h = moveRedRight(h);
+        h.right = deleteMax(h.right);
+        return balance(h);
+    }
+
+    private Node moveRedRight(Node h) {
+        flipColors(h);
+        if (!isRed(root.left.left))
+            h = rotateRight(h);
+        return h;
+    }
+
+    private Node balance(Node h) {
+        if (!isRed(h.left) && isRed(h.right)) h = rotateLeft(h);
+        if (isRed(h.left) && isRed(h.left.left)) h = rotateRight(h);
+        if (isRed(h.left) && isRed(h.right)) flipColors(h);
+        return h;
+    }
+
+    public void delete(Key key) {
+        if (!isRed(root.left) && !isRed(root.right))
+            root.color = RED;
+        root = delete(root, key);
+        if (!isEmpty()) root.color = BLACK;
+    }
+
+    private Node delete(Node h, Key key) {
+        if (key.compareTo(h.key) < 0) {
+            if (!isRed(h.left) && !isRed(h.left.left))
+                h = moveRedLeft(h);
+            h.left = delete(h.left, key);
+        } else {
+            if (isRed(h.left))
+                h = rotateRight(h);
+            if (key.compareTo(h.key) == 0 && h.right == null)
+                return null;
+            if (!isRed(h.right) && !isRed(h.right.left))
+                h = rotateRight(h);
+            if (key.compareTo(h.key) == 0) {
+                h.value = get(h.right, min(h.right).key);
+                h.key = min(h.right).key;
+                h.right = deleteMin(h.right);
+            } else h.right = delete(h.right, key);
+        }
+        return balance(h);
+    }
+
+    public Value get(Key key) {
+        return get(root, key);
+    }
+
+    private Value get(Node h, Key key) {
+        if (key == null) throw new NoSuchElementException();
+        if (h == null) return null;
+        int comp = key.compareTo(h.key);
+        if (comp > 0) return get(h.right, key);
+        else if (comp < 0) return get(h.left, key);
+        else return h.value;
     }
 
     public Key min() {
-        if (isEmpty()) throw new NoSuchElementException();
         return min(root).key;
     }
 
-    private Node min(Node x) {
-        if (x.left == null) return x;
-        return min(x.left);
+    private Node min(Node h) {
+        if (h == null) throw new NoSuchElementException();
+        if (h.left == null) return h;
+        return min(h.left);
     }
 
     public Key max() {
-        if (isEmpty()) throw new NoSuchElementException();
-        return max(root.right).key;
+        return max(root).key;
     }
 
-    private Node max(Node x) {
-        if (x.right == null) return x;
-        return max(x.right);
+    private Node max(Node h) {
+        if (h == null) throw new NoSuchElementException();
+        if (h.right == null) return h;
+        return max(h.right);
     }
 
-    /**
-     * private Node moveRedLeft(Node h) {
-     * //假设h为红色，h.left 和 h.left.left 都是黑色
-     * //将h.left 或者 h.left的子节点之一变红
-     * flipColors(h);
-     * if (isRed(h.right.left)) {
-     * h.right = rotateRight(h.right);
-     * h = rotateLeft(h);
-     * }
-     * return h;
-     * }
-     * <p>
-     * public void deleteMin() {
-     * if (!isRed(root.left) && !isRed(root.left.left))
-     * root.color = RED;
-     * root = deleteMin(root);
-     * if (!isEmpty()) root.color = BLACK;
-     * }
-     * <p>
-     * private Node deleteMin(Node h) {
-     * if (h.left == null) return null;
-     * if (!isRed(h.left) && !isRed(h.left.left))
-     * h = moveRedLeft(h);
-     * h.left = deleteMin(h.left);
-     * return balance(h);
-     * }
-     * <p>
-     * private Node balance(Node h) {
-     * <p>
-     * }
-     */
+    public Iterable<Key> keys() {
+        return keys(min(), max());
+    }
+
+    public Iterable<Key> keys(Key low, Key hi) {
+        Queue<Key> queue = new Queue<>();
+        keys(root, queue, low, hi);
+        return queue;
+    }
+
+    private void keys(Node x, Queue queue, Key lo, Key hi) {
+        if (x == null) return;
+        int comlo = lo.compareTo(x.key);
+        int comhi = hi.compareTo(x.key);
+        if (comlo < 0) keys(x.left, queue, lo, hi);
+        if (comlo <= 0 && comhi >= 0) queue.enqueue(x.key);
+        if (comhi > 0) keys(x.right, queue, lo, hi);
+    }
 
     private class Node {
-
         private Node left;
         private Node right;
+        private boolean color = BLACK;
         private Key key;
         private Value value;
         private int N;
-        private boolean color;
 
-        public Node(Key key, Value value, int n, boolean color) {
+        public Node(Key key, Value value, int N, boolean color) {
             this.key = key;
             this.value = value;
-            N = n;
+            this.N = N;
             this.color = color;
         }
     }
+
 
 }
