7b2752b9e77747f06bc60797b9d023759cd57007
Yif_Corleone <h759233960@163.com>
2018-08-14 22:27:16 +0800

    Chapter4_1 BreadthFirst

---
+++ src/exercise/chapter4_1/BreadthFirstPaths.java
@@ -0,0 +1,46 @@
+package exercise.chapter4_1;
+
+import exercise.chapter1_3.Queue;
+import exercise.chapter1_3.Stack;
+
+public class BreadthFirstPaths {
+    private final int s;        //startup
+    private boolean[] marked;   //vertex marked
+    private int[] edgeTo;       //last vertex in road
+
+    public BreadthFirstPaths(Graph G, int s) {
+        marked = new boolean[G.V()];
+        edgeTo = new int[G.V()];
+        this.s = s;
+        bfs(G, s);
+    }
+
+    private void bfs(Graph G, int s) {
+        Queue<Integer> queue = new Queue<>();
+        marked[s] = true;
+        queue.enqueue(s);
+        while (!queue.isEmpty()) {
+            int v = queue.dequeue();
+            for (int w : G.adj(v)) {
+                if (!marked[w]) {
+                    edgeTo[w] = v;
+                    marked[w] = true;
+                    queue.enqueue(w);
+                }
+            }
+        }
+    }
+
+    public boolean hasPathTo(int v) {
+        return marked[v];
+    }
+
+    public Iterable<Integer> pathTo(int v) {
+        if (!hasPathTo(v)) return null;
+        Stack<Integer> path = new Stack<>();
+        for (int x = v; x != s; x = edgeTo[x])
+            path.push(x);
+        path.push(s);
+        return path;
+    }
+}
