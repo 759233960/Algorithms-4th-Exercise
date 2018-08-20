76af895d5607f2beb2e6488cb136f0386e3a0a93
yif-hong <h759233960@163.com>
2018-08-20 20:39:44 +0800

    chapter 4_1 Connect components

--- src/exercise/chapter4_1/CC.java
+++ src/exercise/chapter4_1/CC.java
@@ -1,4 +1,85 @@
 package exercise.chapter4_1;
 
+import edu.princeton.cs.algs4.In;
+import exercise.chapter1_3.Bag;
+
+/**
+ * 用深度优先搜索找出图中的所有连通分量
+ */
 public class CC {
+    private boolean[] marked;   //已标记的点
+    private int[] id;           //如果v属于第i个连通分量，则 id[v] = i
+    private int count;          //连通分量总数
+
+    public CC(Graph G) {
+        marked = new boolean[G.V()];
+        id = new int[G.V()];
+        for (int s = 0; s < G.V(); s++)
+            if (!marked[s]) {
+                dfs(G, s);
+                count++;
+            }
+    }
+
+    public static void main(String[] args) {
+        Graph G = new Graph(new In(args[0]));
+        CC cc = new CC(G);
+
+        int M = cc.count();
+        System.out.println(M + " components");
+
+        Bag<Integer>[] components;
+        components = (Bag<Integer>[]) new Bag[M];
+        for (int i = 0; i < M; i++)
+            components[i] = new Bag<>();
+        for (int v = 0; v < G.V(); v++)
+            components[cc.id(v)].add(v);
+        for (int i = 0; i < M; i++) {
+            for (int v : components[i])
+                System.out.print(v + " ");
+            System.out.println();
+        }
+    }
+
+    private void dfs(Graph G, int v) {
+        marked[v] = true;
+        id[v] = count;
+        for (int w : G.adj(v))
+            if (!marked[w])
+                dfs(G, w);
+    }
+
+    public boolean connected(int v, int w) {
+        return id[v] == id[w];
+    }
+
+    public int id(int v) {
+        return id[v];
+    }
+
+    public int count() {
+        return count;
+    }
+}
+
+class UnitTest {
+    public static void main(String[] args) {
+        Graph G = new Graph(new In(args[0]));
+        CC cc = new CC(G);
+
+        int M = cc.count();
+        System.out.println(M + " components");
+
+        Bag<Integer>[] components;
+        components = (Bag<Integer>[]) new Bag[M];
+        for (int i = 0; i < M; i++)
+            components[i] = new Bag<>();
+        for (int v = 0; v < G.V(); v++)
+            components[cc.id(v)].add(v);
+        for (int i = 0; i < M; i++) {
+            for (int v : components[i])
+                System.out.print(v + " ");
+            System.out.println();
+        }
+    }
 }
---
+++ src/exercise/chapter4_1/SymbolGraph.java
@@ -0,0 +1,51 @@
+package exercise.chapter4_1;
+
+import edu.princeton.cs.algs4.In;
+import exercise.chapter3_5.ST;
+
+public class SymbolGraph {
+    private ST<String, Integer> st;                  //符号名String   ->  索引int
+    private String[] keys;                           //索引int    ->  符号名String
+    private Graph G;                                 //用于管理邻接表，索引图中的顶点int
+
+    public SymbolGraph(String stream, String sp) {
+        st = new ST<>();
+        In in = new In(stream);                      //第一遍
+        while (in.hasNextLine()) {                   // 构造索引st表
+            String[] a = in.readLine().split(sp);
+
+            for (int i = 0; i < a.length; i++)
+                if (!st.contains(a[i]))
+                    st.put(a[i], st.size());
+        }
+        keys = new String[st.size()];                // 构造反向索引keys
+
+        for (String name : st.keys())
+            keys[st.get(name)] = name;
+
+        G = new Graph(st.size());
+        in = new In(stream);                         //第二遍
+        while (in.hasNextLine()) {                   //构造图
+            String[] a = in.readLine().split(sp);    //将每行的第一个顶点与该行的其他顶点相连
+            int v = st.get(a[0]);
+            for (int i = 0; i < a.length; i++)
+                G.addEdge(v, st.get(a[i]));
+        }
+    }
+
+    public boolean contains(String s) {
+        return st.contains(s);
+    }
+
+    public int index(String s) {
+        return st.get(s);
+    }
+
+    public String name(int v) {
+        return keys[v];
+    }
+
+    public Graph G() {
+        return G;
+    }
+}
