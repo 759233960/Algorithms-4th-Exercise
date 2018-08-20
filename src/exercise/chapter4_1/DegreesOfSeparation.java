2873152de942a4e282d8ac005550a45fa26e3f89
yif-hong <h759233960@163.com>
2018-08-20 20:54:33 +0800

    chapter 4_1 间隔的度数
    用于解决图中两顶点之间已知的最短路径，选择了宽度优先搜索来实现，数据表采用符号图来构建数据。

--- src/exercise/chapter4_1/DegreesOfSeparation.java
+++ src/exercise/chapter4_1/DegreesOfSeparation.java
@@ -4,9 +4,13 @@ import edu.princeton.cs.algs4.StdIn;
 
 public class DegreesOfSeparation {
     public static void main(String[] args) {
+        //arg[0]表示输入流，arg[1]表示分隔符
+        //arg[2]表示起点，arg[3]表示终点
         SymbolGraph sg = new SymbolGraph(args[0], args[1]);
+
         Graph G = sg.G();
-        String source = args[2];                                  //starting point
+        //starting point
+        String source = args[2];
         if (!sg.contains(source)) {
             System.out.println(source + " not in database.");
             return;
@@ -14,7 +18,8 @@ public class DegreesOfSeparation {
         int s = sg.index(source);
         BreadthFirstPaths bfs = new BreadthFirstPaths(G, s);
         while (!StdIn.isEmpty()) {
-            String sink = StdIn.readLine();                     //terminal point
+            //terminal point
+            String sink = StdIn.readLine();
             if (sg.contains(sink)) {
                 int t = sg.index(sink);
                 if (bfs.hasPathTo(t))
