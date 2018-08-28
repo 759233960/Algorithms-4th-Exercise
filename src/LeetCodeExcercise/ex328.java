e877c4c7a6ad27777b6b1277f1b7cd0b9010ae28
yif-hong <h75933960@163.com>
2018-08-28 18:45:40 +0800

    ex328.java 奇偶链表

--- src/LeetCodeExcercise/ex328.java
+++ src/LeetCodeExcercise/ex328.java
@@ -23,8 +23,8 @@ package LeetCodeExcercise;
 public class ex328 {
 
     public ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null) return head;
         ListNode odd = head;
-        if (odd == null || odd.next == null) return head;
         ListNode even = head.next;
         ListNode evenHead = even;
         while (even != null && even.next != null) {
