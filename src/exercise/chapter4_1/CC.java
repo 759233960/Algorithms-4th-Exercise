c3e7d1d4aa1133ab6b85d24c5981a15b62b074da
yif-hong <h759233960@163.com>
2018-08-30 00:41:34 +0800

    CC.java 删除了重复的测试用例

--- src/exercise/chapter4_1/CC.java
+++ src/exercise/chapter4_1/CC.java
@@ -61,25 +61,3 @@ public class CC {
         return count;
     }
 }
-
-class UnitTest {
-    public static void main(String[] args) {
-        Graph G = new Graph(new In(args[0]));
-        CC cc = new CC(G);
-
-        int M = cc.count();
-        System.out.println(M + " components");
-
-        Bag<Integer>[] components;
-        components = (Bag<Integer>[]) new Bag[M];
-        for (int i = 0; i < M; i++)
-            components[i] = new Bag<>();
-        for (int v = 0; v < G.V(); v++)
-            components[cc.id(v)].add(v);
-        for (int i = 0; i < M; i++) {
-            for (int v : components[i])
-                System.out.print(v + " ");
-            System.out.println();
-        }
-    }
-}
