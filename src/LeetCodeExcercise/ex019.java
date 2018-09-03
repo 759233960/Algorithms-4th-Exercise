481cbda9c08a3d5e9da30bb16b691b6ee69f320f
yif-hong <h75933960@163.com>
2018-09-03 14:29:28 +0800

    ex19. 删除链表的倒数第N个节点

---
+++ src/LeetCodeExcercise/ex019.java
@@ -0,0 +1,56 @@
+package LeetCodeExcercise;
+
+/**
+ * 19. 删除链表的倒数第N个节点
+ * 给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
+ * <p>
+ * 示例：
+ * 给定一个链表: 1->2->3->4->5, 和 n = 2.
+ * 当删除了倒数第二个节点后，链表变为 1->2->3->5.
+ * 说明：
+ * 给定的 n 保证是有效的。
+ * <p>
+ * 进阶：
+ * 你能尝试使用一趟扫描实现吗？
+ */
+public class ex019 {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode slow = head;
+        ListNode fast = head;
+        for (int i = 0; i < n; i++)
+            fast = fast.next;
+        if (fast == null) return head.next;
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return head;
+    }
+
+    public ListNode removeNthFromEnd2(ListNode head, int n) {
+        ListNode dummyHead = new ListNode(-1);
+        dummyHead.next = head;
+        ListNode slow = dummyHead;
+        ListNode fast = head;
+        for (int i = 0; i < n; i++)
+            fast = fast.next;
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return dummyHead.next;
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+}
