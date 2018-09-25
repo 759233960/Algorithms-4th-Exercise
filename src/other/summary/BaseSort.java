47e43a1be6f88b5ae735b950730fe1177545c9f2
yif-hong <h75933960@163.com>
2018-09-25 10:57:53 +0800

    修复希尔排序的bug  起始点不对

--- src/other/summary/BaseSort.java
+++ src/other/summary/BaseSort.java
@@ -50,7 +50,7 @@ class ShellSort extends BaseSort {
         int h = 1;
         while (h < a.length / 3) h = h * 3 + 1;
         while (h >= 1) {
-            for (int i = 1; i < a.length; i++)
+            for (int i = h; i < a.length; i++)
                 for (int j = i; j > h && less(a[j], a[j - h]); j -= h)
                     exchange(a, j, j - h);
             h = h / 3;
