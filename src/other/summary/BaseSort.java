3bfb9281d94d32c3f0960b8fa0bb4b048ecc8613
yif-hong <h75933960@163.com>
2018-09-29 16:29:07 +0800

    修改堆排序的错误

--- src/other/summary/BaseSort.java
+++ src/other/summary/BaseSort.java
@@ -63,9 +63,9 @@ abstract class MergeSort extends BaseSort {
 
     void merge(Comparable[] a, int lo, int mid, int hi) {
         int i = lo, j = mid + 1;
-        for (int k = 0; k < a.length; k++)
+        for (int k = lo; k <= hi; k++)
             aux[k] = a[k];
-        for (int k = 0; k < hi; k++) {
+        for (int k = lo; k <= hi; k++) {
             if (i > mid) a[k] = aux[j++];
             else if (j > hi) a[k] = aux[i++];
             else if (less(aux[j], aux[i])) a[k] = aux[j++];
