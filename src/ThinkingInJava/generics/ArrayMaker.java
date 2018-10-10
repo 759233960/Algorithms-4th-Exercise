8646db60e3c0a82af1a986aa23cde0ca9a0bf14e
yif-hong <h759233960@163.com>
2018-10-11 00:07:00 +0800

    upload TIJ Interface of Generics

--- src/ThinkingInJava/generics/ArrayMaker.java
+++ src/ThinkingInJava/generics/ArrayMaker.java
@@ -1,7 +1,6 @@
 package ThinkingInJava.generics;
 
 import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.Arrays;
 
 public class ArrayMaker<T> {
@@ -11,13 +10,13 @@ public class ArrayMaker<T> {
         this.kind = kind;
     }
 
-    T[] create(int size){
-        return (T[]) Array.newInstance(kind,size);
-    }
-
     public static void main(String[] args) {
-        ArrayMaker<String> stringArrayMaker=new ArrayMaker<>(String.class);
+        ArrayMaker<String> stringArrayMaker = new ArrayMaker<>(String.class);
         String[] stringArray = stringArrayMaker.create(9);
         System.out.println(Arrays.toString(stringArray));
+    }
+
+    T[] create(int size) {
+        return (T[]) Array.newInstance(kind, size);
     }
 }
