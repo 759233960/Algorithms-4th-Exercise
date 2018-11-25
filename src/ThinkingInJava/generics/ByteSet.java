fede008c93c9cee6d749b410dc9c689030b353e9
yif-hong <h759233960@163.com>
2018-11-25 14:03:01 +0800

    upload errors in Generics

---
+++ src/ThinkingInJava/generics/ByteSet.java
@@ -0,0 +1,11 @@
+package ThinkingInJava.generics;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ByteSet {
+    Byte[] possibles = {1, 2, 3, 4, 5, 6};
+    Set<Byte> mySet = new HashSet<>(Arrays.asList(possibles));
+//    Set<Byte> mySet2 = new HashSet<>(Arrays.<Byte>asList(1,2,3,4,5,6));
+}
